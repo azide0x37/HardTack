f3e0d4c522a8b71a26ec15c56e0ed444309d2f32
Alexander Templeton <blakmatr@gmail.com>
2015-04-27 02:43:39 -0500

    initial commit!

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+extern crate postgres;
+
+use postgres::{Connection, SslMode};
+
+struct Offender {
+    doc_id: i32,
+    name: String,
+}
+
+fn main() {
+    let conn = Connection::connect("postgres://postgres@localhost", &SslMode::None).unwrap();
+
+    conn.execute("CREATE TABLE offenders (
+    doc_id   SERIAL PRIMARY KEY,
+    name    VARCHAR NOT NULL
+    )", &[]).unwrap();
+
+    let me = Offender {
+        doc_id: 666666,
+        name: "Friggin' the devil, okay?".to_string()
+    };
+
+    conn.execute("INSERT INTO offenders (doc_id, name) VALUES ($1, $2)", &[&me.doc_id, &me.name]).unwrap();
+
+    let stmt = conn.prepare("SELECT id, name FROM offenders").unwrap();
+    for row in stmt.query(&[]).unwrap() {
+        let offender = Offender {
+            doc_id: row.get(0),
+            name: row.get(1)
+        };
+        println!("Found person {}", offender.name);
+    }
+}
